e16edbe09cd2bb99ed234dccb23c8cfc1dbf6070
hazekat2 <thomas.hazekamp2@mail.dcu.ie>
2023-12-24 00:08:10 +0000

    #1 initial setup rn app

---
+++ src/mobile_react_native/forknfind-mobile/app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home Test</Text>
+        </View>
+    );
+}
+
+export default Home;
