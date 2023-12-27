f924b2fc4cfb42ed1c00f5c821639fd0c7aecefe
dalye54 <eoin.daly54@mail.dcu.ie>
2023-12-27 14:07:41 +0000

    Login and Logout functionality. Related #2

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
+    path('', include('forknfind.urls')),
     path('admin/', admin.site.urls),
+    path('api_auth/', include('rest_framework.urls')), # Apit auth token urls
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
