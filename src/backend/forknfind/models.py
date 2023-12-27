78a687154035b6def36e935d60322bae40116d30
dalye54 <eoin.daly54@mail.dcu.ie>
2023-12-27 15:18:47 +0000

    Methods for the APIUser class. Related #2

--- src/backend/forknfind/models.py
+++ src/backend/forknfind/models.py
@@ -3,4 +3,21 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class APIUser(AbstractUser):
-    pass
+    
+    def get_username(self):
+        return self.username
+    
+    def get_firstName(self):
+        return self.first_name
+    
+    def get_lastName(self):
+        return self.last_name
+    
+    def get_email(self):
+        return self.email
+    
+    def get_full_name(self):
+        return self.first_name() + " " + self.last_name()
+    
+    def __str__(self):
+        return f'Username: {self.get_username()}\nFirst Name: {self.get_firstName()}\nLast Name: {self.get_lastName()}\nEmail: {self.get_email()}'
