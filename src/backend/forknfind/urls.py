5f53b630796f3388a157f97b676a26cec05f428b
dalye54 <eoin.daly54@mail.dcu.ie>
2023-12-28 15:26:02 +0000

    Review registration functionality added. Related #7

--- src/backend/forknfind/urls.py
+++ src/backend/forknfind/urls.py
@@ -5,9 +5,11 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
 router.register(r'restaurant', RestaurantViewSet)
+router.register(r'review', ReviewViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
     path('register/user/', UserRegistrationAPIView.as_view(), name="api_register_user"),
-    path('register/restaurant/', RestaurantRegistrationAPIView.as_view(), name="api_register_restaurant")
+    path('register/restaurant/', RestaurantRegistrationAPIView.as_view(), name="api_register_restaurant"),
+    path('register/review/', ReviewRegistrationAPIView.as_view(), name="api_register_review"),
 ]
