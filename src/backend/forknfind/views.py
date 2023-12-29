53b66cc23d59c7d44b72d28c6f5e0c0a5f1b6987
dalye54 <eoin.daly54@mail.dcu.ie>
2023-12-29 18:48:30 +0000

    Updated response to return more information relating to each restaurant. Related #11

--- src/backend/forknfind/views.py
+++ src/backend/forknfind/views.py
@@ -60,7 +60,7 @@ class RestaurantsAroundUserAPIView(APIView):
         for item in queryset:
             distance = haversine((float(longitude), float(latitude)), item.get_location())
             if distance < 2:
-                within_distance[item.get_name()] = distance
+                within_distance[item.get_name()] = {'distance':distance, 'location':item.get_location(), 'rating':item.get_average_rating()}
 
 
         return Response(within_distance, status=status.HTTP_200_OK)
