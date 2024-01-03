638d3da5512eb33a3692782e58c594fad7628334
hazekat2 <thomas.hazekamp2@mail.dcu.ie>
2024-01-03 23:31:24 +0000

    Created initial navigation bar. Closes #23

--- src/mobile_react_native/forknfind-mobile/app/home.js
+++ src/mobile_react_native/forknfind-mobile/app/home.js
@@ -1,11 +1,89 @@
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text, StatusBar, Pressable } from 'react-native';
+import React from 'react';
 
-const Home = () => {
-    return (
-        <View>
-            <Text>Home</Text>
-        </View>
-    );
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Entypo } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
+/*
+    References: Nav bar design & logic - https://www.youtube.com/watch?v=g14lCxkasWc&ab_channel=Indently
+*/
+
+export default class Home extends React.Component {
+
+    state = {
+        screenText: 'Press a button!'
+    }
+
+    changeText = (text) => {
+        console.log(text + ' has been pressed');
+        this.setState({ 
+            screenText: text 
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Text style={{fontSize:30, color:'white'}}>{this.state.screenText}</Text>
+                </View>
+
+                <View style={styles.NavContainer}>
+                    <View style={styles.NavBar}>
+
+                    <Pressable onPress={() => this.changeText('Reviews')} style={styles.IconBehaviour}>
+                    <MaterialIcons name="rate-review" size={24} color="black" />
+                    </Pressable>
+
+                    <Pressable onPress={() => this.changeText('Recomendations')} style={styles.IconBehaviour}>
+                    <Entypo name="new" size={24} color="black" />
+                    </Pressable>
+
+                    <Pressable onPress={() => this.changeText('Location')} style={styles.IconBehaviour}>
+                    <Entypo name="location" size={24} color="black" />
+                    </Pressable>
+
+                    <Pressable onPress={() => this.changeText('Search')} style={styles.IconBehaviour}>
+                    <FontAwesome name="search" size={24} color="black" />
+                    </Pressable>
+
+                    <Pressable onPress={() => this.changeText('Settings')} style={styles.IconBehaviour}>
+                    <Ionicons name="settings" size={24} color="black" />
+                    </Pressable>
+
+                    </View>
+
+                </View>
+            </View>
+        );
+    }
 }
 
-export default Home;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent:'center',
+    },
+
+    NavContainer: {
+        position: 'absolute',
+        alignItems: 'center',
+        bottom: 20,
+    },
+
+    NavBar: {
+        flexDirection: 'row',
+        backgroundColor: '#eee',
+        width: '90%',
+        justifyContent: 'space-evenly',
+        borderRadius: 40,
+    },
+
+    IconBehaviour: {
+        padding: 14,
+    }
+});
