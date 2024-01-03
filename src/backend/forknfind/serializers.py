2cb062aaf0ac388e278c137de554a639f0ab1ba7
dalye54 <eoin.daly54@mail.dcu.ie>
2024-01-03 13:09:22 +0000

    Updated Restaurant Serializer to include the hours attribute. Related #14

--- src/backend/forknfind/serializers.py
+++ src/backend/forknfind/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Restaurant
-        fields = ['id','google_id','longitude','latitude','name','address','type','price_level','allows_dogs','delivery','dine_in','good_for_children','good_for_groups','outdoor_seating','average_rating']
+        fields = ['id','google_id','longitude','latitude','name','address','type','price_level','allows_dogs','delivery','dine_in','good_for_children','good_for_groups','outdoor_seating', 'hours', 'average_rating']
 
 class ReviewSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
