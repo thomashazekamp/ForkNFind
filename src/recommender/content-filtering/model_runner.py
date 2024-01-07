fd28e508620944e59b227e44b14da43731602dd2
dalye54 <eoin.daly54@mail.dcu.ie>
2024-01-07 21:31:21 +0000

    TF-IDF model using each of the 3 datasets. Fixes #33

--- src/recommender/content-filtering/model_runner.py
+++ src/recommender/content-filtering/model_runner.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from baseline_model import *
 from sklearn_model import *
+from tfidf_model import *
 
 def main():
 
@@ -12,10 +13,10 @@ def main():
 
     baseline_score = baseline(business_data)
 
-    sklearn_score_categorys = cosine_similarity_model_categorys(business_data)
-    sklearn_score_attributes = cosine_similarity_model_attributes(business_data)
-    sklearn_score_categorys_and_attributes = cosine_similarity_model_categorys_and_attributes(business_data)
+    sklearn_score_categorys, sklearn_score_attributes, sklearn_score_categorys_and_attributes  = cosine_similarity_models(business_data)
+    tfidf_score_categorys, tfidf_score_attributes, tfidf_score_categorys_and_attributes = tf_idf_similarity_models(business_data)
 
-    print(f'Baseline: {baseline_score}\nSklearn - Categorys: {sklearn_score_categorys}\nSklearn - Attributes: {sklearn_score_attributes}\nSklearn - Categorys & Attributes: {sklearn_score_categorys_and_attributes}')
+    print(f'Baseline: {baseline_score:.5f}\nSklearn - Categorys: {sklearn_score_categorys:.5f}\nSklearn - Attributes: {sklearn_score_attributes:.5f}\nSklearn - Categorys & Attributes: {sklearn_score_categorys_and_attributes:.5f}')
+    print(f'Baseline: {baseline_score:.5f}\nTF-IDF - Categorys: {tfidf_score_categorys:.5f}\nTF-IDF - Attributes: {tfidf_score_attributes:.5f}\nTF-IDF - Categorys & Attributes: {tfidf_score_categorys_and_attributes:.5f}')
 
 main()
