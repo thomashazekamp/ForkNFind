5a5900ff08d5b38293e5d66bd53538045e6f0e96
dalye54 <eoin.daly54@mail.dcu.ie>
2024-01-07 20:32:06 +0000

    Updated code to reduce the repetition

--- src/recommender/content-filtering/sklearn_model.py
+++ src/recommender/content-filtering/sklearn_model.py
@@ -3,12 +3,10 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import math
 
 def get_similar_items(row, cosine_similarity_matrix, train, test_ids):
 
     id = int(row[0])
-    original_rating = row[1]
     
     item_similarities = cosine_similarity_matrix[id]
 
@@ -24,82 +22,63 @@ def get_similar_items(row, cosine_similarity_matrix, train, test_ids):
                 predicted_rating.append(result_row['stars'].item())
 
         # this represents the k nearest neighbour value
-        if len(predicted_rating) == 5:
+        if len(predicted_rating) == 7:
             break
 
-    return sum(predicted_rating) / 5
+    return sum(predicted_rating) / 7
 
-def cosine_similarity_model_categorys(data):
+def compute_similarity_and_loop(data, rating_train, rating_test, feature_columns):
 
-    ratings = data[['business_id', 'stars']]
+    features = data[feature_columns]
+   
+    cosine_similarity_matrix = cosine_similarity(features)
 
-    rating_train, rating_test = train_test_split(ratings, test_size=0.2, random_state=38)
+    new_ratings = []
 
-    rating_train.to_csv("rating_train.csv")
-    rating_test.to_csv("rating_test.csv")
+    for _, row in rating_test.iterrows():
+        new_ratings.append(get_similar_items(row, cosine_similarity_matrix, rating_train, rating_test['business_id']))
 
-    feature_columns = data.columns.tolist()
+    return new_ratings
+
+
+def cosine_similarity_model_categorys(data, rating_train, rating_test, feature_columns):
+
     feature_columns = feature_columns[7:46]
 
-    features = data[feature_columns]
-
-    features.to_csv("test.csv")
-    
-    cosine_similarity_matrix = cosine_similarity(features)
-
-    new_ratings = []
-
-    for index, row in rating_test.iterrows():
-        new_ratings.append(get_similar_items(row, cosine_similarity_matrix, rating_train, rating_test['business_id']))
+    new_ratings = compute_similarity_and_loop(data, rating_train, rating_test, feature_columns)
 
     return mean_squared_error(new_ratings, rating_test['stars'], squared=False)
 
-def cosine_similarity_model_attributes(data):
 
-    ratings = data[['business_id', 'stars']]
+def cosine_similarity_model_attributes(data, rating_train, rating_test, feature_columns):
 
-    rating_train, rating_test = train_test_split(ratings, test_size=0.2, random_state=38)
-
-    rating_train.to_csv("rating_train.csv")
-    rating_test.to_csv("rating_test.csv")
-
-    feature_columns = data.columns.tolist()
     feature_columns = feature_columns[46:]
 
-    features = data[feature_columns]
-
-    features.to_csv("test.csv")
-    
-    cosine_similarity_matrix = cosine_similarity(features)
-
-    new_ratings = []
-
-    for index, row in rating_test.iterrows():
-        new_ratings.append(get_similar_items(row, cosine_similarity_matrix, rating_train, rating_test['business_id']))
+    new_ratings = compute_similarity_and_loop(data, rating_train, rating_test, feature_columns)
 
     return mean_squared_error(new_ratings, rating_test['stars'], squared=False)
 
-def cosine_similarity_model_categorys_and_attributes(data):
+
+def cosine_similarity_model_categorys_and_attributes(data, rating_train, rating_test, feature_columns):
+
+    feature_columns = feature_columns[7:]
+
+    new_ratings = compute_similarity_and_loop(data, rating_train, rating_test, feature_columns)
+
+    return mean_squared_error(new_ratings, rating_test['stars'], squared=False)
+
+
+def cosine_similarity_models(data):
 
     ratings = data[['business_id', 'stars']]
 
     rating_train, rating_test = train_test_split(ratings, test_size=0.2, random_state=38)
 
-    rating_train.to_csv("rating_train.csv")
-    rating_test.to_csv("rating_test.csv")
-
     feature_columns = data.columns.tolist()
-    feature_columns = feature_columns[7:]
 
-    features = data[feature_columns]
+    sklearn_score_categorys = cosine_similarity_model_categorys(data, rating_train, rating_test, feature_columns)
+    sklearn_score_attributes = cosine_similarity_model_attributes(data, rating_train, rating_test, feature_columns)
+    sklearn_score_categorys_and_attributes = cosine_similarity_model_categorys_and_attributes(data, rating_train, rating_test, feature_columns)
 
-    features.to_csv("test.csv")
-    
-    cosine_similarity_matrix = cosine_similarity(features)
 
-    new_ratings = []
-
-    for index, row in rating_test.iterrows():
-        new_ratings.append(get_similar_items(row, cosine_similarity_matrix, rating_train, rating_test['business_id']))
-
-    return mean_squared_error(new_ratings, rating_test['stars'], squared=False)
+    return sklearn_score_categorys, sklearn_score_attributes, sklearn_score_categorys_and_attributes 
