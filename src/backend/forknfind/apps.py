b9244f30337232ec684e4fabd6b0a79f996d8bad
dalye54 <eoin.daly54@mail.dcu.ie>
2024-01-15 13:06:08 +0000

    Comments added for apps.py

--- src/backend/forknfind/apps.py
+++ src/backend/forknfind/apps.py
@@ -9,10 +9,10 @@ class ForknfindConfig(AppConfig):
 
         time.sleep(1) # wait for 1 seconds
 
+        # Import the recommendation model
         from .models import HybridRecommender
 
+        # Call the recommendation model, as it is a singleton model can just call it using .load()
         model = HybridRecommender.load()
+        # Start the recommender using its method .start_recommender()
         model.start_recommender()
-        #model.query_content_recommender(151)
-        #model.query_collaborative_recommender(1)
-        #model.query_hybrid_recommender(1)
