98ba8cc6f197332aefe3154757d5925661253399
dalye54 <eoin.daly54@mail.dcu.ie>
2024-02-09 16:05:01 +0000

    New API request for changing user password. Related #103

---
+++ src/mobile_react_native/forknfind-mobile/app/navigation/requests/ChangePasswordAPIRequest.js
@@ -0,0 +1,32 @@
+
+// Function ChangePasswordAPIRequest
+// currentPassword - user's current password
+// newPassword - user's new password
+// setResponseData - saving response if it is a success
+const ChangePasswordAPIRequest = (currentPassword, newPassword, setResponseData) => {
+
+    console.log("does this work?")
+    console.log(currentPassword, newPassword)
+
+    fetch("https://192.168.1.82:8000/user/password/update/", 
+    {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_global,
+    },
+    body: JSON.stringify({current_password: currentPassword, new_password: newPassword})
+    }).then(response=>response.json())
+    .then(data=>{
+
+        console.log(data)
+        setResponseData(data)
+    })
+    .catch(error => {
+        console.error('Network request failed:', error);
+      })
+    ;
+}  
+
+export default ChangePasswordAPIRequest;
