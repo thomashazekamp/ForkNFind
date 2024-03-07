d6f7c37988283e95d92f1b6e63a07d825ccd93c7
hazekat2 <thomas.hazekamp2@mail.dcu.ie>
2024-03-07 12:47:42 +0000

    Created option to run model and save the model for better performance. Relates to #124

--- src/sentiment_analysis/runner_sent_analysis.py
+++ src/sentiment_analysis/runner_sent_analysis.py
@@ -6,6 +6,9 @@ from dataset_worker import read_dataset, apply_dataset_column_modifications, dat
 from models_processing import run_model
 from txt_processing import process_text
 
+# Importing necessary libraries
+from joblib import dump, load
+
 # Process the data by calling all relative dataset processing functions
 def process_data():
     # Read dataset
@@ -21,7 +24,19 @@ def process_data():
 
 # Predict the sentiment of a given text
 def predict_sentiment(text, vectorization_type='tfidf'):
-    model, text_trans = run_model(vectorization_type) # returns the model and the text transformer, can have input such as: 'cv' or 'tfidf' (uses tfidf as default option) to choose which vectorization type to use
+    run_save_model = True
+    model_file_name = 'sentiment_analysis_model.pkl'
+
+    if run_save_model == True:
+        model, text_trans = run_model(vectorization_type) # returns the model and the text transformer, can have input such as: 'cv' or 'tfidf' (uses tfidf as default option) to choose which vectorization type to use
+
+        # Save the model and text transformer
+        dump((model, text_trans), model_file_name)
+
+    try:
+        model, text_trans = load(model_file_name) # load the model and text transformer
+    except FileNotFoundError:
+        print("Model file not found. Please initially run the model to save it.")
 
     processed_text = process_text(text) # process the text - remove links, convert emojis to text, remove hashtags, make string lowercase, remove repeated characters and punctuation and replace contractions
     new_text = text_trans.transform([processed_text]) # transform the processed text - using the text transformer
@@ -38,7 +53,8 @@ def predict_sentiment(text, vectorization_type='tfidf'):
 def main():
     # This can be used for example purposes
     example_text = "I am happy, this is great!"
-    print(predict_sentiment(example_text))
+    example_text2 = "I am sad, this is terrible!"
+    print(predict_sentiment(example_text2))
 
 if __name__ == '__main__':
     main()
