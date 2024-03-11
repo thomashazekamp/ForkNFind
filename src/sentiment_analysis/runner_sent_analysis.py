ca28db71ae7aa69e468878a3a9caf912ee0f8b95
hazekat2 <thomas.hazekamp2@mail.dcu.ie>
2024-03-11 11:17:30 +0000

    Added ability to choose model from runner & updated model pkl file. Relates to #122

--- src/sentiment_analysis/runner_sent_analysis.py
+++ src/sentiment_analysis/runner_sent_analysis.py
@@ -27,20 +27,21 @@ def process_data():
     return x, y
 
 # Predict the sentiment of a given text
-def predict_sentiment(text, vectorization_type='tfidf'):
+def predict_sentiment(text, vectorization_type='tfidf', model_type='logistic_regression'):
     # Print the current time
     print(f'Current time at start: {datetime.now()}')
-    run_save_model = False # Last updated: 07/3/2024
+
+    run_save_model = True # Last updated: 07/3/2024
     model_file_name = 'sentiment_analysis_model.pkl'
 
-    if os.path.isfile(model_file_name):
+    if os.path.isfile(model_file_name) and (run_save_model == False):
         # Load saved model with error handling
         try:
             model, text_trans = load(model_file_name) # load the model and text transformer
         except FileNotFoundError:
             print("Model file not found. Please initially run the model to save it.")
     else:
-        model, text_trans = run_model(vectorization_type) # returns the model and the text transformer, can have input such as: 'cv' or 'tfidf' (uses tfidf as default option) to choose which vectorization type to use
+        model, text_trans = run_model(vectorization_type, model_type) # returns the model and the text transformer, can have input such as: 'cv' or 'tfidf' (uses tfidf as default option) to choose which vectorization type to use
         # Save the model and text transformer
         dump((model, text_trans), model_file_name)
 
