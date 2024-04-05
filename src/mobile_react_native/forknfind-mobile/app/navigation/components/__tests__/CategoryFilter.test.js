0aca205f23767c3a64b14c91060ff3e65cd488f7
hazekat2 <thomas.hazekamp2@mail.dcu.ie>
2024-04-05 12:14:32 +0100

    Added working unit tests for files. Relates to #133

--- src/mobile_react_native/forknfind-mobile/app/navigation/components/__tests__/CategoryFilter.test.js
+++ src/mobile_react_native/forknfind-mobile/app/navigation/components/__tests__/CategoryFilter.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, waitFor } from '@testing-library/react-native';
 import CategoryFilter from '../CategoryFilter';
 
 describe('CategoryFilter', () => {
     // mock categories
     const mockCategories = ['testChinese', 'testIndian', 'testItalian'];
 
-    it('renders Categories text', () => {
-        const { getByText } = render(<CategoryFilter selectedCategories={mockCategories}/>);
+    it('renders Categories text', async () => {
+        const { getByText, findByText } = render(<CategoryFilter selectedCategories={mockCategories}/>);
+        await waitFor(() => getByText('Categories'), {timeout: 30000});
+        await findByText('Categories');
         expect(getByText('Categories')).toBeTruthy();
+    });
+
+    it('Should create a snapshot', () => {
+        const tree = render(<CategoryFilter selectedCategories={mockCategories}/>).toJSON();
+        expect(tree).toMatchSnapshot();
     });
 });
--- src/mobile_react_native/forknfind-mobile/app/navigation/components/__tests__/CreateReviewScreen.test.js
+++ src/mobile_react_native/forknfind-mobile/app/navigation/components/__tests__/CreateReviewScreen.test.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, waitFor } from '@testing-library/react-native';
 import CreateReviewScreen from '../CreateReviewScreen';
+import { create } from 'react-test-renderer';
 
 describe('CreateReviewScreen', () => {
     // mock the restaurant data, needed for the screen
     const mockRestaurantData = { name: 'Test Restaurant' };
+
+    it('Should have 3 children', () => {
+        // check that the screen has 3 children
+        const tree = create(<CreateReviewScreen restaurantData={mockRestaurantData} />).toJSON();
+        expect(tree.children.length).toBe(3);
+    });
+
+    it('Should have a snapshot', () => {
+        // check that the screen matches the snapshot
+        const tree = create(<CreateReviewScreen restaurantData={mockRestaurantData} />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
     
-    it('Should have a Submit', () => {
+    it('Should have a Submit', async () => {
         // render the review screen with a given restaurant name
         const { getByText } = render(<CreateReviewScreen restaurantData={mockRestaurantData} />);
+        // wait for the network request to complete
+        await waitFor(() => getByText('Your Rating'), {timeout: 10000});
         // check that the screen has rendered
         expect(getByText('Submit')).toBeTruthy();
     });
-    it('Should have a adding review', () => {
+    it('Should have a adding review', async () => {
         // render the review screen with a given restaurant name
         const { getByText } = render(<CreateReviewScreen restaurantData={mockRestaurantData} />);
+        // wait for the network request to complete
+        await waitFor(() => getByText('Your Rating'), {timeout: 10000});
         // check that the screen has rendered
         expect(getByText('Add detailed Review')).toBeTruthy();
     });
-    it('Should have your rating', () => {
+    it('Should have your rating', async () => {
         // render the review screen with a given restaurant name
         const { getByText } = render(<CreateReviewScreen restaurantData={mockRestaurantData} />);
+        // wait for the network request to complete
+        await waitFor(() => getByText('Your Rating'), {timeout: 10000});
         // check that the screen has rendered
         expect(getByText('Your Rating')).toBeTruthy();
     });
