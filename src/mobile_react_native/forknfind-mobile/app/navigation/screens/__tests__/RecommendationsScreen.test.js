e3b04ff2e56556e126a8374e7936744022468fba
hazekat2 <thomas.hazekamp2@mail.dcu.ie>
2024-04-06 17:40:57 +0100

    Added test for the screens. Fixes #133

---
+++ src/mobile_react_native/forknfind-mobile/app/navigation/screens/__tests__/RecommendationsScreen.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, waitFor } from '@testing-library/react-native';
+import { create } from 'react-test-renderer';
+import RecommendationsScreen from '../RecommendationsScreen';
+
+describe('RecommendationsScreen', () => {
+
+    global.access_global = 'token_test' // used to mock the access token
+
+    it('Should have 2 children', () => {
+        // check that the screen has 3 children
+        const tree = create(<RecommendationsScreen />).toJSON();
+        expect(tree.children.length).toBe(2);
+    });
+
+    it('Should have a snapshot', () => {
+        // check that the screen matches the snapshot
+        const tree = create(<RecommendationsScreen />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
---
+++ src/mobile_react_native/forknfind-mobile/app/navigation/screens/__tests__/SettingScreen.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, waitFor } from '@testing-library/react-native';
+import { create } from 'react-test-renderer';
+import SettingsScreen from '../SettingsScreen';
+
+describe('SettingsScreen', () => {
+
+    global.Platform = 'ios' // used to mock the platform
+    global.access_global = 'token_test' // used to mock the access token
+
+    it('Should have 2 children', () => {
+        // check that the screen has 3 children
+        const tree = create(<SettingsScreen />).toJSON();
+        expect(tree.children.length).toBe(5);
+    });
+
+    it('Should have a snapshot', () => {
+        // check that the screen matches the snapshot
+        const tree = create(<SettingsScreen />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+    
+    it('Should have Account Details', () => {
+        // render the review screen with a given data
+        const { getByText } = render(<SettingsScreen />);
+        // check that the screen has rendered
+        expect(getByText('Account Details')).toBeTruthy();
+    });
+
+    it('Should have Change Password', () => {
+        // render the review screen with a given data
+        const { getByText } = render(<SettingsScreen />);
+        // check that the screen has rendered
+        expect(getByText('Change Password')).toBeTruthy();
+    });
+
+    it('Should have Logout', () => {
+        // render the review screen with a given data
+        const { getByText } = render(<SettingsScreen />);
+        // check that the screen has rendered
+        expect(getByText('Logout')).toBeTruthy();
+    });
+});
