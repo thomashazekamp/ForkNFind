adcfa6d48b88c64b28106aa70d38f6c288b37ff7
dalye54 <eoin.daly54@mail.dcu.ie>
2024-04-10 20:17:39 +0100

    Ignoring warning that pops up on app startup

--- src/mobile_react_native/forknfind-mobile/app/index.js
+++ src/mobile_react_native/forknfind-mobile/app/index.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import MainContainer from './navigation/MainContainer';
 import { NavigationContainer } from '@react-navigation/native';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['Require cycle:']);
 
 function App() {
 
